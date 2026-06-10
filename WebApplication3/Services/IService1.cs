775dc85f7c7e99528398197fc3ffacac603249f9
unknown <jayden.aurelus@ushunger.com>
2026-06-10 16:10:40 +0000

    Initliazing new solution w testing

---
+++ WebApplication3/Services/IService1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.ServiceModel.Activation;
+using System.Text;
+
+
+namespace WebApplication2.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+
+    public interface IService1
+    {
+        [OperationContract]
+
+        [WebGet(UriTemplate = "getAllApplicants", BodyStyle = WebMessageBodyStyle.Bare, ResponseFormat = WebMessageFormat.Json)]
+        string GetAllApplicants();
+        /*
+        [OperationContract]
+        [WebGet(UriTemplate = "/getApplicantByID/{id}", ResponseFormat = WebMessageFormat.Json)]
+        dynamic GetApplicantByID(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/createApplicant", ResponseFormat = WebMessageFormat.Json)]
+        void CreateApplicant();
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT", UriTemplate = "/editApplicant", ResponseFormat = WebMessageFormat.Json)]
+        void EditApplicant();
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE", UriTemplate = "deleteContact", ResponseFormat = WebMessageFormat.Json)]
+        void DeleteApplicant(string ID);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "test")]
+        string Test();
+        */
+        //Operation Contract 
+        //WebGet/Invoke 
+        //Funcion
+    }
+}
---
+++ WebApplication3/Services/Service1.svc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)]
+    public class Service1 : IService1
+    {
+        //private internDBEntities db = new internDBEntities();
+        public string GetAllApplicants()
+        {
+            var applicants = "AppliANT";
+
+            return applicants;
+        }
+       /* public mockData GetApplicantByID(string ID)
+
+        {
+
+            return db.mockDatas.FirstOrDefault();
+        }
+        public void CreateApplicant() { return; }
+        public void EditApplicant() { return; }
+        public void DeleteApplicant(string ID)
+        {
+            return;
+        }
+        public string Test()
+        {
+            return "working";
+        }*/
+    }
+}
